7bab2d11baece8df1bbe71b3422a8ba6a920625a
Hammed Lawal <lawalolatunde14@gmail.com>
2025-12-25 14:14:01 -0500

    chore(user): scaffold User class

---
+++ car-booking-system/src/com/devlawal/user/User.java
@@ -0,0 +1,46 @@
+package com.devlawal.user;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class User {
+    private String name;
+    private UUID id;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+}
