baa48661055c9447a7cb4c0fcd527e1d728c70b4
Hammed Lawal <lawalolatunde14@gmail.com>
2026-01-15 16:56:36 -0500

    refactor(Main.java): instantiated all needed classes

--- car-booking-system/src/com/devlawal/Main.java
+++ car-booking-system/src/com/devlawal/Main.java
@@ -1,15 +1,24 @@
 package com.devlawal;
 
+import com.devlawal.booking.BookingDao;
 import com.devlawal.booking.BookingService;
+import com.devlawal.car.CarDao;
 import com.devlawal.car.CarService;
+import com.devlawal.user.UserArrayAccessDataService;
+import com.devlawal.user.UserDao;
+import com.devlawal.user.UserFileAccessDataService;
 import com.devlawal.user.UserService;
 import static com.devlawal.MainService.userInput;
 
 public class Main {
     public static void main(String[] args) {
-        BookingService bookingService = new BookingService();
-        UserService userService = new UserService();
-        CarService carService = new CarService();
+        UserDao fileUserDao = new UserFileAccessDataService();
+        UserDao arrayUserDao = new UserArrayAccessDataService();
+        UserService userService = new UserService(fileUserDao, arrayUserDao);
+        CarDao carDao = new CarDao();
+        CarService carService = new CarService(carDao);
+        BookingDao bookingDao = new BookingDao();
+        BookingService bookingService = new BookingService(bookingDao, carService, userService);
         userInput(userService, bookingService, carService);
     }
 }
