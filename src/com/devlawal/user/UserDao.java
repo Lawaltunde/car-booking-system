ee2b63620f97b33c95892150547c31bc0b3e180f
Hammed Lawal <lawalolatunde14@gmail.com>
2026-01-28 15:02:05 -0500

    refactor(UserDao.java): deletUser method was made default to avoid duplicating codes

--- src/com/devlawal/user/UserDao.java
+++ src/com/devlawal/user/UserDao.java
@@ -4,8 +4,40 @@ import java.util.UUID;
 
 public interface UserDao {
 
-        User[] getUsers();
-        boolean addUser(User user);
-        void deleteUser(UUID userId);
+    User[] getUsers();
+
+    boolean addUser(User user);
+
+    default void deleteUser(UUID userId, User[] users) {
+        if (userId == null) {
+            throw new IllegalArgumentException("id can't be null");
+        }
+
+        if (users == null || users.length == 0) {
+            // nothing to delete
+            return;
+        }
+
+        int pos = -1;
+        for (int i = 0; i < users.length; i++) {
+            User user = users[i];
+            if (user != null && user.getId() != null && user.getId().equals(userId)) {
+                pos = i;
+                users[i] = null;
+                break;
+            }
+        }
+
+        if (pos == -1) {
+            // not found, there is nothing to delete
+            return;
+        }
+
+        // shift left to remove the null hole
+        for (int i = pos; i < (users.length - 1); i++) {
+            users[i] = users[i + 1];
+        }
+        users[users.length - 1] = null;
+    }
 
 }
--- src/com/devlawal/user/UserFileAccessDataService.java
+++ src/com/devlawal/user/UserFileAccessDataService.java
@@ -3,8 +3,6 @@ package src.com.devlawal.user;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.UUID;
-
 public class UserFileAccessDataService implements UserDao {
     private static User[] users;
 
@@ -46,39 +44,6 @@ public class UserFileAccessDataService implements UserDao {
         moreUsers[users.length] = user;
         users = moreUsers;
         return true;
-    }
-
-    @Override
-    public void deleteUser(UUID userId) {
-        if (userId == null) {
-            throw new IllegalArgumentException("id can't be null");
-        }
-
-        if (users == null || users.length == 0) {
-            // nothing to delete
-            return;
-        }
-
-        int pos = -1;
-        for (int i = 0; i < users.length; i++) {
-            User user = users[i];
-            if (user != null && user.getId() != null && user.getId().equals(userId)) {
-                pos = i;
-                users[i] = null;
-                break;
-            }
-        }
-
-        if (pos == -1) {
-            // not found, there is nothing to delete
-            return;
-        }
-
-        // shift left to remove the null hole
-        for (int i = pos; i < (users.length - 1); i++) {
-            users[i] = users[i + 1];
-        }
-        users[users.length - 1] = null;
     }
 
     private static User[] getUsersFromFile(String filePath) {
