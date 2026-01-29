b2e9c6dc577f6db94ddebfa6485f155338320d53
Hammed Lawal <lawalolatunde14@gmail.com>
2026-01-29 10:35:06 -0500

    fix(UserFileAccessDataService.java): ensured user is initialized before invoking .add method

--- src/com/devlawal/user/UserFileAccessDataService.java
+++ src/com/devlawal/user/UserFileAccessDataService.java
@@ -2,7 +2,7 @@ package src.com.devlawal.user;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 public class UserFileAccessDataService implements UserDao {
@@ -30,36 +30,37 @@ public class UserFileAccessDataService implements UserDao {
     }
 
     private static List<User> getUsersFromFile(String filePath) {
+        List<User>localUsers = new ArrayList<>();
         File file = new File(filePath);
         if (!file.exists()) {
             throw new IllegalStateException("User file not found: " + filePath);
         }
+        List<User> users = new ArrayList<>();
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                if (line != null && !line.trim().isEmpty()) {
-                    line = scanner.nextLine().trim();
-                    String[] parts = line.split(",");
+                if (line.isEmpty()) {
+                    continue;
+                }
+                String[] parts = line.split(",");
                     if (parts.length != 3) {
                         // skip lines
                         // I will modify this later to maybe add random id to a data missing id
                         continue;
                     }
-                    String name = parts[0].trim();
-                    String email = parts[1].trim();
-                    String ageStr = parts[2].trim();
-                    int age;
                     try {
-                        age = Integer.parseInt(ageStr);
+                        String name = parts[0].trim();
+                        String email = parts[1].trim();
+                        String ageStr = parts[2].trim();
+                        int age = Integer.parseInt(ageStr);
+
+                        localUsers.add(new User(name, email, age));
                     } catch (NumberFormatException e) {
-                        // skip lines with invalid age, I don't want anyone below 18 to drive
                         System.out.println(e.getMessage());
                         continue;
                     }
-                    users.add(new User(name, email, age));
-                }
             }
-            return users;
+            return localUsers;
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
