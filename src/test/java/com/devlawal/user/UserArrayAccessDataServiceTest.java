a714b37158a4d7cd281bf2b128a6d4c5c9bf812f
Hammed Lawal <lawalolatunde14@gmail.com>
2026-02-12 13:23:49 -0500

    set up junit dependencies

---
+++ src/test/java/com/devlawal/user/UserArrayAccessDataServiceTest.java
@@ -0,0 +1,14 @@
+package com.devlawal.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserArrayAccessDataServiceTest {
+
+    @Test
+    void name() {
+        System.out.println("Hellow");
+    }
+
+}
