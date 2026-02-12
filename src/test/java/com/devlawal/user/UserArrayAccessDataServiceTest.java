c05be0c19a987d14c923d1ba057c97de890d3ba4
Hammed Lawal <lawalolatunde14@gmail.com>
2026-02-12 17:42:13 -0500

    test(UserArrayAccessDataServiceTest): tested UserArrayAccessDataService's methods

--- src/test/java/com/devlawal/user/UserArrayAccessDataServiceTest.java
+++ src/test/java/com/devlawal/user/UserArrayAccessDataServiceTest.java
@@ -1,14 +1,43 @@
 package com.devlawal.user;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class UserArrayAccessDataServiceTest {
+    private UserDao underTest;
+    private User user;
+
+
+    @BeforeEach
+    void setUp() {
+        underTest = new UserArrayAccessDataService();
+        user = new User("Ademola", "Ademola@drive.com", 29);
+    }
 
     @Test
-    void name() {
-        System.out.println("Hellow");
+    void canAddUsers() {
+        boolean actual = underTest.addUser(user);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void cannotAddUsers() {
+        boolean actual = underTest.addUser(null);
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    void canReturnAlUsers() {
+        int actual = underTest.getUsers().size();
+        assertThat(underTest.getUsers().size()).isEqualTo(4);
+    }
+
+    @Test
+    void cannotReturnAlUsers() {
+        int actual = underTest.getUsers().size();
+        assertThat(actual).isNotEqualTo(2);
     }
 
 }
